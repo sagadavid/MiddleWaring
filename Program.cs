817ecca768da36ddf425680d49a055e931ad43ff
David Saga <epostsaga@gmail.com>
2024-05-26 19:57:25 +0200

    middleware, logic next up and down next

--- Program.cs
+++ Program.cs
@@ -23,6 +23,44 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+app.Use(async (context, next) =>
+{
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
+    logger.LogInformation($"request host : {context.Request.Host}");
+    logger.LogInformation("middleware 0 - before");
+
+    await next(context);
+
+    logger.LogInformation("middleware 0 - after");
+    logger.LogInformation($"response status kode: {context.Response.StatusCode}");
+});
+
+app.Use(async (context, next) =>
+{
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogInformation($"ClientName HttpHeader in Middleware 1: {context.Request.Headers["ClientName"]}");
+    logger.LogInformation($"Add a ClientName HttpHeader in Middleware 1");
+    context.Request.Headers.TryAdd("ClientName", "Machos");
+    logger.LogInformation("My Middleware 1 - Before");
+    await next(context);
+    logger.LogInformation("My Middleware 1 - After");
+    logger.LogInformation($"Response StatusCode in Middleware 1: {context.Response.StatusCode}");
+});
+
+app.Use(async (context, next) =>
+{
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogInformation($"ClientName HttpHeader in Middleware 2: {context.Request.Headers["ClientName"]}");
+    logger.LogInformation("My Middleware 2 - Before");
+    context.Response.StatusCode = StatusCodes.Status202Accepted;
+    await next(context);
+    logger.LogInformation("My Middleware 2 - After");
+    logger.LogInformation($"Response StatusCode in Middleware 2: {context.Response.StatusCode}");
+});
+
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
