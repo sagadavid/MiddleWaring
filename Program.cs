f6bb043067b80b5182b71f6dab8f970dc9f45463
David Saga <epostsaga@gmail.com>
2024-05-28 16:52:13 +0200

    indre scope order regner

--- Program.cs
+++ Program.cs
@@ -23,17 +23,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
+//3 middleware components implements own logic and passes next, statements run according to scopes inner order, nemlig: after 2 line 1, line 2, after 1, line 1, line 2
 app.Use(async (context, next) =>
 {
     var logger = app.Services.GetRequiredService<ILogger<Program>>();
 
     logger.LogInformation($"request host : {context.Request.Host}");
-    logger.LogInformation("middleware 0 - before");
+    logger.LogInformation("Before Next 0");
 
     await next(context);
 
-    logger.LogInformation("middleware 0 - after");
+    logger.LogInformation("After Next 0");
     logger.LogInformation($"response status kode: {context.Response.StatusCode}");
 });
 
@@ -43,9 +43,9 @@ app.Use(async (context, next) =>
     logger.LogInformation($"ClientName HttpHeader in Middleware 1: {context.Request.Headers["ClientName"]}");
     logger.LogInformation($"Add a ClientName HttpHeader in Middleware 1");
     context.Request.Headers.TryAdd("ClientName", "Machos");
-    logger.LogInformation("My Middleware 1 - Before");
+    logger.LogInformation("Before Next 1");
     await next(context);
-    logger.LogInformation("My Middleware 1 - After");
+    logger.LogInformation("After Next 1");
     logger.LogInformation($"Response StatusCode in Middleware 1: {context.Response.StatusCode}");
 });
 
@@ -53,10 +53,10 @@ app.Use(async (context, next) =>
 {
     var logger = app.Services.GetRequiredService<ILogger<Program>>();
     logger.LogInformation($"ClientName HttpHeader in Middleware 2: {context.Request.Headers["ClientName"]}");
-    logger.LogInformation("My Middleware 2 - Before");
+    logger.LogInformation("Before Next 2");
     context.Response.StatusCode = StatusCodes.Status202Accepted;
     await next(context);
-    logger.LogInformation("My Middleware 2 - After");
+    logger.LogInformation("After 2");
     logger.LogInformation($"Response StatusCode in Middleware 2: {context.Response.StatusCode}");
 });
 
